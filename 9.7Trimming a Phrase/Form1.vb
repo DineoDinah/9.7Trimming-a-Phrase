61c0c21be5d227f6db2681d7846d31ac4700efd4
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-04 22:02:52 +0200

    Add project files.

---
+++ 9.7Trimming a Phrase/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+		Dim myPhrase As String
+
+		myPhrase = InputBox("Enter your phrase")
+		TextBox1.Text = Trim(myPhrase) 'Trims the empty spaces on both sides of the phrase
+
+	End Sub
+End Class
